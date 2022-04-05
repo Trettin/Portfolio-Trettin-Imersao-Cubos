8a3598e169e0cbaf252846a188f1b0c797e16288
Gabriel Trettin <gtcastro7@gmail.com>
2022-04-05 09:07:10 -0300

    fix: changed year of last update in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,5 +4,5 @@ import { useTranslation } from "react-i18next";
 export default function Footer() {
   const { t } = useTranslation();
 
-  return <footer>Tretin.Dev@gmail.com | {t("last-update")} 2021</footer>;
+  return <footer>Tretin.Dev@gmail.com | {t("last-update")} 2022</footer>;
 }
